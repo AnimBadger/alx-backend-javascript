17623d3ddc36676422f6cf2efac636a311b61853
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-25 22:05:31 +0000

    format files

---
+++ 0x00-ES6_basic/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = [
+        [
+            '@babel/preset-env',
+            {
+                targets: 'last 2 versions, > 1%',
+            },
+        ],
+    ];
+
+    return {
+        presets,
+    };
+};
