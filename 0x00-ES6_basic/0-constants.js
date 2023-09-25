d7e193407f4b5838d54ad9cfad34d77ebb854bf2
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-25 21:28:38 +0000

    task 0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+};
+
+export function getLast () {
+  return ' is okay';
+}
+
+export const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+};
