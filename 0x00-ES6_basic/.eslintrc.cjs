236d0747375ade5e1f2d3edb791ffb431ab65046
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-26 12:14:46 +0000

    task 6

--- 0x00-ES6_basic/.eslintrc.cjs
+++ 0x00-ES6_basic/.eslintrc.cjs
@@ -1,26 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'eslint:recommended',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'comma-dangle': ['error', 'always']
+  }
+};
--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,4 +1,4 @@
-export default function getSanFranciscoDescription () {
+export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
     income: '$119,868',
