1160f856c9c250cd81b120f993531026d3dc7a46
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-26 12:51:49 +0000

    task 8

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -3,7 +3,7 @@ function getCurrentYear () {
   return date.getFullYear()
 }
 
-export default function getBudgetForCurrentYear (income, gdp, capita) {
+export default function getBudgetForCurrentYear (income, gdp, capita,) {
   return {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
