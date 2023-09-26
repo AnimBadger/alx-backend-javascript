9282469070914332d14ea37a5834e2dbefec617d
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-26 14:43:26 +0000

    package files

--- 0x00-ES6_basic/babel.config.js
+++ 0x00-ES6_basic/babel.config.js
@@ -1,16 +1,12 @@
-module.exports = function (api) {
-    api.cache(true);
-
-    const presets = [
-        [
-            '@babel/preset-env',
-            {
-                targets: 'last 2 versions, > 1%',
-            },
-        ],
-    ];
-
-    return {
-        presets,
-    };
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
 };
