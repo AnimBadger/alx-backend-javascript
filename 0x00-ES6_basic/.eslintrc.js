20200112a4a080550418d67cf58c18c0c8a2d0ac
AnimBadger <anim.ansah.stephen@gmail.com>
2023-09-26 14:51:43 +0000

    package file

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -2,27 +2,27 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
-    jest: true
+    jest: true,
   },
   extends: ['airbnb-base', 'plugin:jest/all'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['jest'],
   rules: {
     'no-console': 'off',
     'no-shadow': 'off',
-    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement']
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
   },
   overrides: [
     {
       files: ['*.js'],
-      excludedFiles: 'babel.config.js'
-    }
-  ]
+      excludedFiles: 'babel.config.js',
+    },
+  ],
 };
