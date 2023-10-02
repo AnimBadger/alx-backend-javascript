78d255e402acc30dc80c9a61a63dc67562f34348
AnimBadger <anim.ansah.stephen@gmail.com>
2023-10-02 15:07:26 +0000

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents() {
+  const studentsList = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+  return studentsList;
+}
