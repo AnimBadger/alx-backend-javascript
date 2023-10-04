7a90f6805cd2da954b776ebd8c82e35f02ab3488
AnimBadger <anim.ansah.stephen@gmail.com>
2023-10-04 14:36:16 +0000

    task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,6 +2,6 @@ export default function getStudentsByLocation(listOfStudents, city) {
   if (!Array.isArray(listOfStudents) && typeof city !== 'string') {
     return [];
   }
-  const SanFranciscoStudents = listOfStudents.filter((obj) => obj.location === 'San Francisco');
+  const SanFranciscoStudents = listOfStudents.filter((obj) => obj.location === city);
   return SanFranciscoStudents;
 }
