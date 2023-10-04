4611b33a23f3a8851aba9cbfdbc811c970cb2823
AnimBadger <anim.ansah.stephen@gmail.com>
2023-10-04 15:06:32 +0000

    task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+const createInt8TypedArray = (length, position, value) => {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const newBuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(newBuffer, 0, length);
+  int8.set([value], position);
+  return new DataView(newBuffer);
+};
+
+export default createInt8TypedArray;
